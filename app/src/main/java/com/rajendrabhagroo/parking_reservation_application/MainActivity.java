7f2d6de784d3019a154ff803e8bad9ad1b6a3527
RajRays <Raj@Fameria.com>
2018-03-08 14:35:54 -0500

    Initial Commit

---
+++ app/src/main/java/com/rajendrabhagroo/parking_reservation_application/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rajendrabhagroo.parking_reservation_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
