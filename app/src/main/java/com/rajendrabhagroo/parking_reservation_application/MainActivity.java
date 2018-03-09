8c155db12006405a9ae5b34ccc0f00ea53fc6da2
RajRays <Raj@Fameria.com>
2018-03-08 20:23:49 -0500

    Firebase Analytics Included

--- app/src/main/java/com/rajendrabhagroo/parking_reservation_application/MainActivity.java
+++ app/src/main/java/com/rajendrabhagroo/parking_reservation_application/MainActivity.java
@@ -3,12 +3,20 @@ package com.rajendrabhagroo.parking_reservation_application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain FirebaseAnalytics Instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
     }
 
 }
