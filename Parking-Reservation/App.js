e8bc3904b264c99e84f4d0d0af16bf26bc204061
RajRays <Raj@Fameria.com>
2018-03-12 23:31:04 -0400

    Re-Structured Project For React-Native

---
+++ Parking-Reservation/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Parking Reservation App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
