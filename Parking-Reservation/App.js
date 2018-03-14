5e85a86651d776e615444c3a00302417f0182214
RajRays <Raj@Fameria.com>
2018-03-13 21:01:24 -0400

    Log-In View Developed

--- Parking-Reservation/App.js
+++ Parking-Reservation/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+/* FireBase */
 import * as firebase from 'firebase';
 
+/* Custom Components */
+import { Input } from './src/components/Input';
+import { Button } from './src/components/Button';
+
 export default class App extends React.Component {
+
+  state = {
+    email: '',
+    password: '',
+  }
 
   componentWillMount(){
     const firebaseConfig = {
@@ -15,8 +26,25 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Parking Reservation App</Text>
+      <View style = {styles.container}>
+
+        <Input
+          placeholder = 'Enter Email Here ... '
+          label = 'Email'
+          onChangeText = {email => this.setState({ email })}
+          value = {this.state.email}
+        />
+
+        <Input
+          placeholder = 'Enter Password Here ... '
+          label = 'Password'
+          secureTextEntry
+          onChangeText = {password => this.setState({ password })}
+          value = {this.state.password}
+        />
+
+        <Button onPress = {() => console.log('Pressed') } >Log In</Button>
+
       </View>
     );
   }
@@ -25,8 +53,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
--- Parking-Reservation/src/components/Input.js
+++ Parking-Reservation/src/components/Input.js
@@ -1,50 +1,51 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput } from 'react-native';
 
-const input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
-    return {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+    return (
 
-        <View style = {style.container}>
-        <Text>Label</Text>
+        <View style = {styles.container}>
+        <Text style = {styles.label}>{ label }</Text>
         
-        <TextInput
+        <TextInput style = {styles.input}
 
         autoCorrect = {false}
         onChangeText = {onChangeText}
         placeholder = {placeholder}
-        style= {styles.input}
+        style = {styles.input}
         secureTextEntry = {secureTextEntry}
         value = {value}
 
         />
         </View>
-    }
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         marginTop: 10,
-        witdth: '100%',
-        border: '#eee',
+        borderColor: '#eee',
         borderBottomWidth: 2,
+        width: '100%',
     },
 
     label: {
         padding: 5,
         paddingBottom: 0,
-        color: '#333',
         fontSize: 17,
         fontWeight: '700',
+        color: '#333',
         width: '100%',
     },
 
     input: {
-        paddingRight: 5,
         paddingLeft: 5,
-        paddingBottom: 2,
-        color:
-        fontSize:
-        fontWeight:
-        width:
+        paddingRight: 5,
+        paddingBottom: 10,
+        fontSize: 18,
+        color: '#333',
+        width: '100%',
     }
-})
+});
+
+export { Input };
